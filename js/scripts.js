26839ef5ab41b6a5130d8a28f0178fc9ba9bb5f9
MirkoCifelli <ciffo4@hotmail.it>
2024-01-09 15:26:56 +0100

    Add some code

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,45 @@
-const { createApp } = Vue;
 
-// Creo l'istanza di Vue da mondare in pagina
-createApp({
+const {createApp} = Vue;
+
+createApp ({
     data() {
         return {
-            message: 'Template HTML CSS Vue',
+            newTodo:'',
+            todos: [
+                {
+                text: 'TheFinals',
+                done: true,
+                 },
+                {
+                text: 'Valorant',
+                done: true,
+                },
+                {
+                text: 'Apex Legends',
+                done: true,
+                },
+                {
+                text: 'Fortnite',
+                done: false,
+                }
+            ]
         };
+    },
+    methods: {
+        addTodo() {
+            if (this.newTodo.trim().length > 0) {
+                let newElem={};
+                newElem.text = this.newTodo;
+                newElem.done = false
+                // this.todos.push(this.newTodo.trim());
+                // this.newTodo = '';
+                this.todos.push(newElem);
+                this.newTodo= '';
+            }
+        },
+        removeTodo(i) {
+            console.log(i, this.todos[i]);
+            this.todos.splice(i, 1);
+        }
     }
-  // Monto l'istanza di Vue in pagina
 }).mount('#app');
