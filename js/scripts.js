85942b3a777032b6cf97ae748937b058797a4d99
MirkoCifelli <ciffo4@hotmail.it>
2024-01-09 13:39:55 +0100

    Inizializzato progetto

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,11 @@
-console.log(`hello world`)
+const { createApp } = Vue;
+
+// Creo l'istanza di Vue da mondare in pagina
+createApp({
+    data() {
+        return {
+            message: 'Template HTML CSS Vue',
+        };
+    }
+  // Monto l'istanza di Vue in pagina
+}).mount('#app');
